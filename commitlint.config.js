ac1f8188cccbcb224acb15180cd0bd131aaa6cd1
Louis Fredice NJAKO MOLOM <131726886+luifr10@users.noreply.github.com>
2023-05-17 10:56:45 +0200

    fix: remove explicit plugin @semantic-release/git and just set commit message, #117

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,20 @@
 module.exports = {
     extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+    rules: {
+        "footer-max-line-length": [1, "always", 100],
+        "type-enum": [2, "always", [
+            "build",
+            "chore",
+            "ci",
+            "docs",
+            "feat",
+            "fix",
+            "perf",
+            "refactor",
+            "revert",
+            "style",
+            "test",
+            "release"
+        ]],
+    }
 };
