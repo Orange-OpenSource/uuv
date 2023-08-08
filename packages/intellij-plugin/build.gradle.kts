72a948f99bdef6416becca395af158db86213816
Louis Fredice NJAKO MOLOM <131726886+luifr10@users.noreply.github.com>
2023-08-08 14:29:26 +0200

    chore(intellij-plugin): setup pipeline for intellij-plugin project, #195

--- packages/intellij-plugin/build.gradle.kts
+++ packages/intellij-plugin/build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
   id("java")
   id("org.jetbrains.kotlin.jvm") version "1.8.21"
-  id("org.jetbrains.intellij") version "1.13.3"
+  id("org.jetbrains.intellij") version "1.15.0"
 }
 
 group = "com.e2e-testing.uuv"
-version = "1.0-SNAPSHOT"
 
 repositories {
   mavenCentral()
@@ -36,12 +35,13 @@ tasks {
   }
 
   signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    certificateChain.set(providers.environmentVariable("UUV_CERTIFICATE_CHAIN"))
+    privateKey.set(providers.environmentVariable("UUV_PRIVATE_KEY"))
+    password.set(providers.environmentVariable("UUV_PRIVATE_KEY_PASSWORD"))
+    inputArchiveFile.set(file(buildPlugin.get().archiveFileName))
   }
 
   publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
+    token.set(providers.environmentVariable("UUV_PUBLISH_TOKEN"))
   }
 }
