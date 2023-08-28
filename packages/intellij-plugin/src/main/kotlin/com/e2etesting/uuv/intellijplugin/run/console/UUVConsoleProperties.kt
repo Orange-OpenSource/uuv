c764d79025ddf2b250444531bb00bd16beb21b3d
Louis Fredice NJAKO MOLOM <131726886+luifr10@users.noreply.github.com>
2023-08-28 20:39:34 +0200

    fix(intellij-plugin): set flag to display passed tests, #234

--- packages/intellij-plugin/src/main/kotlin/com/e2etesting/uuv/intellijplugin/run/console/UUVConsoleProperties.kt
+++ packages/intellij-plugin/src/main/kotlin/com/e2etesting/uuv/intellijplugin/run/console/UUVConsoleProperties.kt
@@ -2,12 +2,16 @@ package com.e2etesting.uuv.intellijplugin.run.console
 
 import com.intellij.execution.Executor
 import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.execution.testframework.TestConsoleProperties
 import com.intellij.execution.testframework.sm.runner.SMTRunnerConsoleProperties
 import com.intellij.execution.testframework.sm.runner.SMTestLocator
 import com.intellij.openapi.util.NlsSafe
 import org.jetbrains.annotations.NotNull
 
 class UUVConsoleProperties(@NotNull config: RunConfiguration, @NlsSafe @NotNull testFrameworkName: String, @NotNull executor: Executor) : SMTRunnerConsoleProperties(config, testFrameworkName, executor) {
+    init {
+        this.setValueOf(TestConsoleProperties.HIDE_PASSED_TESTS, false)
+    }
     override fun getTestLocator(): SMTestLocator {
         return UUVTestLocator.INSTANCE
     }
