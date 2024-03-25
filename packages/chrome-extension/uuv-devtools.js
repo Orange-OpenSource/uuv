2268f6d766e562bb7a972072aeff64b1e8eb7066
Stanley S <32981008+stanlee974@users.noreply.github.com>
2024-03-25 15:56:44 +0100

    chore: update spdx header on source file, #558 (#559)

--- packages/chrome-extension/uuv-devtools.js
+++ packages/chrome-extension/uuv-devtools.js
@@ -1,17 +1,16 @@
 /**
-* Software Name : UUV
-*
-* SPDX-FileCopyrightText: Copyright (c) 2022-2024 Orange
-* SPDX-License-Identifier: MIT
-*
-* This software is distributed under the MIT License,
-* the text of which is available at https://spdx.org/licenses/MIT.html
-* or see the "LICENSE" file for more details.
-*
-* Authors: NJAKO MOLOM Louis Fredice & SERVICAL Stanley
-* Software description: Make test writing fast, understandable by any human
-* understanding English or French.
-*/
+ * Software Name : UUV
+ *
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT License,
+ * see the "LICENSE" file for more details
+ *
+ * Authors: NJAKO MOLOM Louis Fredice & SERVICAL Stanley
+ * Software description: Make test writing fast, understandable by any human
+ * understanding English or French.
+ */
 
 chrome.devtools.inspectedWindow.eval("setSelectedElement($0)", { useContentScriptContext: true }, (result, exceptionInfo) => {
     console.log('result');
